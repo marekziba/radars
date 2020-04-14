aa7dd474618b2de7fa4f3ba95293a57690b59201
Marek Zięba <marek.zieba@icloud.com>
2020-04-14 22:27:58 +0200

    Updates

---
+++ precache-manifest.a38301c6663973830adfd7ba6e8d809f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f87ad4b6f1adc287fffbaeb16ef095",
+    "url": "/radars/index.html"
+  },
+  {
+    "revision": "0adbf6a5dd5af378eaa0",
+    "url": "/radars/static/css/main.1a3d811f.chunk.css"
+  },
+  {
+    "revision": "fb2affc7af84e11287fd",
+    "url": "/radars/static/js/2.3370451e.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/radars/static/js/2.3370451e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0adbf6a5dd5af378eaa0",
+    "url": "/radars/static/js/main.3f506a5f.chunk.js"
+  },
+  {
+    "revision": "75d51258550e37c839ee",
+    "url": "/radars/static/js/runtime-main.592651a3.js"
+  }
+]);
